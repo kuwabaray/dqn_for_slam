35c8d6e81f34299f15edf108bcb13a817a631ca6
yukiya@gcp_ros-dqn <yukiya104@gmail.com>
2021-02-03 19:45:47 +0000

    edit to run py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
+NAME = 'dqn_for_slam'
 REQUIRES_PYTHON = '>=3.5.0'
 
 setup(
-    name="dqn_for_slam",
+    name=NAME,
     version='0.0.1',
     description='reinforce learning for the robot',
+    packages=find_packages(),
     python_requires=REQUIRES_PYTHON,
     install_requires=[
         'gym==0.17.3',
@@ -15,5 +17,10 @@ setup(
         'keras-rl2==1.0.4',
         'numpy==1.18.5',
         'rospkg==1.2.9'
-    ]
-)
+    ],
+    entry_points={
+        'console_scripts': [
+        	'run_agent=dqn_for_slam.rl_worker:main'
+        ],
+    }
+)  
